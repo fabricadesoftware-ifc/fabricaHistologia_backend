1a188e9f7a10e720b5b1f49b8ac8ea0f62e22dc1
lucasantonete@gmail.com <lucasantonete@gmail.com>
2024-06-30 15:50:10 -0300

    FEAT: Initials configs and user add

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'core.usuario',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = "usuario.Usuario"
--- django_project/urls.py
+++ django_project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from core.usuario.router import router as usuario_router
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api/", include(usuario_router.urls)),
 ]
