e8a15d06d83cdd47bebdd34cdb89e608f83eb5b0
lucasantonete@gmail.com <lucasantonete@gmail.com>
2024-07-02 15:37:22 -0300

    FEAT: quiz app and add models

---
+++ core/quiz/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from core.fabrica_histologia.models import System
+
+# Create your models here.
+
+class Quiz(models.Model):
+    question = models.TextField()
+    level = models.CharField(max_length=255)
+    system = models.ForeignKey(System, on_delete=models.PROTECT)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    option = models.TextField()
+    correct = models.BooleanField()
+
+
