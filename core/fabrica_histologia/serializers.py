cfb35c18fc3bcbc4fbf54f2f9e00629057f61d42
jaotarzan <joaovssouza59@gmail.com>
2024-07-04 16:11:12 -0300

    feat:  create of SlideMicroscopyPostDetailSerializer #11

--- core/fabrica_histologia/serializers.py
+++ core/fabrica_histologia/serializers.py
@@ -1,4 +1,4 @@
-from core.fabrica_histologia.models import System
+from core.fabrica_histologia.models import SlideMicroscopyPost, System 
 from rest_framework import serializers
 
 class SystemDetailSerializer(serializers.ModelSerializer):
@@ -18,4 +18,20 @@ class SystemWriteSerializer(serializers.ModelSerializer):
             "name",
             "description",
             "image_system",
+        ]
+
+class SlideMicroscopyPostDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SlideMicroscopyPost
+        fields: list[str] = [
+            "id",
+            "date_analysis",
+            "post_date",
+            "species",
+            "type_cut",
+            "increase",
+            "coloring",
+            "image_slide",
+            "autor_user",
+            "organ",
         ]
