58ee0d062e6fc5ae9b0b2367e513cfbfb05d5694
jaotarzan <joaovssouza59@gmail.com>
2024-07-05 16:42:55 -0300

    feat: aditioning new serializers to SlideMicroscopyPostViewSet #11

--- core/fabrica_histologia/views.py
+++ core/fabrica_histologia/views.py
@@ -1,4 +1,4 @@
-from core.fabrica_histologia.serializers import SlideMicroscopyPostDetailSerializer, SystemDetailSerializer, SystemWriteSerializer
+from core.fabrica_histologia.serializers import SlideMicroscopyPostDetailSerializer, SlideMicroscopyPostListSerializer, SlideMicroscopyPostWriteSerializer, SystemDetailSerializer, SystemWriteSerializer
 from core.fabrica_histologia.models import SlideMicroscopyPost, System
 from rest_framework.viewsets import ModelViewSet
 from drf_spectacular.utils import extend_schema
@@ -20,5 +20,9 @@ class SlideMicroscopyPostViewSet(ModelViewSet):
     queryset = SlideMicroscopyPost.objects.all()
     
     def get_serializer_class(self):
-        return SlideMicroscopyPostDetailSerializer
+        if self.action in ["list"]:            
+            return SlideMicroscopyPostListSerializer
+        elif self.action in ["retrieve"]:
+            return SlideMicroscopyPostDetailSerializer
+        return SlideMicroscopyPostWriteSerializer
     http_method_names = ["get", "post", "put", "delete"]
