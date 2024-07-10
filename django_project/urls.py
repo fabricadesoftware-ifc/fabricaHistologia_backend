0229924bf2ee5173751e561014e27c18fc8df8ef
Laíssa Mendes <laissa.mendes2006@gmail.com>
2024-07-10 15:51:28 -0300

    FEAT: create urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,13 +21,21 @@ from django.conf.urls.static import static
 from core.usuario.router import router as usuario_router
 from core.usuario.views import verify_user
 from core.uploader.router import router as uploader_router
+from core.fabrica_histologia.views import PointViewSet
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r"points", PointViewSet) 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include(usuario_router.urls)),
     path("api/verify-user/<str:verification_token>/", verify_user, name="verify-user"),
     path("api/media/", include(uploader_router.urls)), 
+    path("api/", include(router.urls)),
+    path("", include(router.urls))
 ]
 
 urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
