5dcfb42941c0893a9af2faef608d540a84ad4bc2
jaotarzan <joaovssouza59@gmail.com>
2024-09-13 21:10:59 -0300

    Turning the filter to the system id

--- src/core/veterinary/filters.py
+++ src/core/veterinary/filters.py
@@ -2,8 +2,8 @@ import django_filters
 from core.veterinary.models import Organ
 
 class OrganFilter(django_filters.FilterSet):
-    system_name = django_filters.CharFilter(field_name='system__name', lookup_expr='icontains')
+    system_id = django_filters.NumberFilter(field_name='system__id', lookup_expr='icontains')
 
     class Meta:
         model = Organ
-        fields = ['system_name']
+        fields = ['system_id']
