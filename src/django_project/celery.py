27456632561a5921048f30fc3dc65faa7a971e88
lucasantonete@gmail.com <lucasantonete@gmail.com>
2024-10-31 14:59:05 -0300

    add configs for celery

--- src/django_project/celery.py
+++ src/django_project/celery.py
@@ -6,7 +6,9 @@ from django.conf import settings
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
-app = Celery('django_project', broker='amqp://jaotarzan:Batata_12@localhost:5672/fabricahistologia')
+BROKER_URL = os.getenv('BROKER_URL', 'amqp://jaotarzan:Batata_12@localhost:5672/fabricahistologia')
+
+app = Celery('django_project', broker=BROKER_URL)
 
 app.conf.update(
     broker_connection_retry_on_startup=True,
