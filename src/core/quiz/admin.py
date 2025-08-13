5fa677f010e6ec39a2c6404f9e2e8dcdf0542fbb
Jaotarzan <joaovssouza59@gmail.com>
2025-08-13 15:03:08 -0300

    FEAT: Adicionar modelo Score e suas configurações; atualizar registros no admin, filtros, serializers e views

--- src/core/quiz/admin.py
+++ src/core/quiz/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from core.quiz.models import Answer, Quiz
+from core.quiz.models import Answer, Quiz, Score
 
 # Register your models here.
 admin.site.register(Answer)
 admin.site.register(Quiz)
+admin.site.register(Score)
